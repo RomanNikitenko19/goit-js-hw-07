fd13d6f184c451aa76bb59531a31f335e755968c
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2021-11-11 19:20:44 +0200

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
+//import * as basicLightbox from "basiclightbox";
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  divGallery: document.querySelector(".gallery"),
+};
+
+const markupGallery = galleryItems
+  .map((galleryItem) => {
+    return `<div class="gallery__item">
+              <a class="gallery__link" href="${galleryItem.original}">
+                <img class="gallery__image"src="${galleryItem.preview}"
+                  data-source="${galleryItem.original}"
+                  alt="${galleryItem.description}"
+                  />
+              </a>
+            </div>`;
+  }).join("");
+
+ref.divGallery.innerHTML = markupGallery;
+
+function handler(event) {
+  event.preventDefault();
+  console.log(event.target.dataset.source);
+
+  if (event.target !== event.currentTarget) {
+    const imgRef = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${imgRef}">
+    `);
+
+    instance.show();
+  }
+}
+
+ref.divGallery.addEventListener("click", handler);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const ref = {
+  divGallery: document.querySelector(".gallery"),
+};
+
+const markupGallery = galleryItems
+  .map((galleryItem) => {
+    return `  <a class="gallery__link" href="${galleryItem.original}">
+                <img class="gallery__image"src="${galleryItem.preview}"
+                  alt="${galleryItem.description}"
+                  />
+              </a>`
+  })
+  .join("");
+
+ref.divGallery.innerHTML = markupGallery;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */ captionsData: "alt",
+  animationSpeed: 400,
+});
